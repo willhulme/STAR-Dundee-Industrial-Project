d6df5eab8fa799fb2748799ad6838e1d23b8ce7b
Will Hulme <wjh@outlook.com>
2016-09-16 14:26:21 +0100

    Polished CRC8. Commented and tested working as a private function with returning error codes.

---
+++ Star-Dundee-WPF/Star-Dundee-WPF/Models/Checkmate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Star_Dundee_WPF.Models
+{
+    class Checkmate
+    {
+        CRC8 CRC = new CRC8();
+        private bool lenCheck(string cargo)
+        {
+            CRC.Check(cargo);
+            return true;
+        }
+    }
+}

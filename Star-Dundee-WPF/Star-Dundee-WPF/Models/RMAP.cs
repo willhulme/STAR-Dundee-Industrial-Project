b9acc375aaf117d9dc83b54646e3a81446b607dc
RyanRobinson <RyanRobinson@computing.dundee.ac.uk>
2016-09-17 00:07:50 +0100

    rmap packet breaker upper thing

--- Star-Dundee-WPF/Star-Dundee-WPF/Models/RMAP.cs
+++ Star-Dundee-WPF/Star-Dundee-WPF/Models/RMAP.cs
@@ -15,13 +15,18 @@ namespace Star_Dundee_WPF.Models
         string command { get; set; }
         byte destinationKey { get; set; }
         byte sourceLAdd { get; set; }
-        ushort transactionID { get; set; }
+        //ushort transactionID { get; set; }
+        byte[] transactionID = new byte[2];
         byte extWriteAdd { get; set; }
+        byte extReadAdd { get; set; }
         uint writeAddress { get; set; }
-        uint dataLength { get; set; }
+        uint readAddress { get; set; }
+        byte[] dataLength = new byte[3];
+        uint dataLengthInt { get; set; }
         byte[] data { get; set; }
         byte headerCRC { get; set; }
         byte dataCRC { get; set; }
+        byte replyCRC { get; set; }
         byte reserved { get; set; }
         byte status { get; set; }
 
@@ -33,9 +38,95 @@ namespace Star_Dundee_WPF.Models
             command = getCommandType(ptCmdSp);         
         }
 
+        public void buildPacket(string packet)
+        {
+            string[] characters = packet.Split(' ');
+            byte[] characterBytes = characters.Select(s => Convert.ToByte(s, 16)).ToArray();
+            ptCmdSp = characterBytes[2];
+            command = getCommandType(ptCmdSp);
+
+            if (command.Contains("REPLY"))
+            {
+                status = characterBytes[3];
+                destinationlogicalAddress = characterBytes[4];
+                transactionID[0] = characterBytes[5];
+                transactionID[1] = characterBytes[6];
+
+                if(command.Equals("WRITE REPLY"))
+                {
+                    replyCRC = characterBytes[7];
+                    return;
+                }
+                dataLength[0] = characterBytes[8];
+                dataLength[1] = characterBytes[9];
+                dataLength[2] = characterBytes[10];
+                dataLengthInt = arrayToInt(dataLength);
+                headerCRC = characterBytes[11];
+                data = new byte[dataLengthInt];
+                int j = 0;
+                for(int i = 12; i < characterBytes.Length-1; i++)
+                {
+                    data[j] = characterBytes[i];
+                }
+                dataCRC = characterBytes[(characterBytes.Length - 1)];
+            }
+            else
+            {
+                destinationKey = characterBytes[3];
+                sourcelogicalAddress = characterBytes[4];
+                transactionID[0] = characterBytes[5];
+                transactionID[1] = characterBytes[6];
+
+                if (command.Equals("WRITE"))
+                {
+                    extWriteAdd = characterBytes[7];
+                    byte[] writeBytes = new byte[4] { characterBytes[8], characterBytes[9], characterBytes[10], characterBytes[11] };
+                    writeAddress = arrayToInt(writeBytes);
+                    dataLength[0] = characterBytes[12];
+                    dataLength[1] = characterBytes[13];
+                    dataLength[2] = characterBytes[14];
+                    dataLengthInt = arrayToInt(dataLength);
+                    headerCRC = characterBytes[15];
+                    data = new byte[dataLengthInt];
+                    int j = 0;
+                    for (int i = 16; i < characterBytes.Length - 1; i++)
+                    {
+                        data[j] = characterBytes[i];
+                    }
+                    dataCRC = characterBytes[(characterBytes.Length - 1)];
+                }
+                else if (command.Equals("READ"))
+                {
+                    extReadAdd = characterBytes[7];
+                    byte[] readBytes = new byte[4] { characterBytes[8], characterBytes[9], characterBytes[10], characterBytes[11] };
+                    readAddress = arrayToInt(readBytes);
+                    dataLength[0] = characterBytes[12];
+                    dataLength[1] = characterBytes[13];
+                    dataLength[2] = characterBytes[14];
+                    dataLengthInt = arrayToInt(dataLength);
+                    headerCRC = characterBytes[15];
+                }
+            }
+            printPacketDetails(this);
+
+        }
+
+        private uint arrayToInt(byte[] dataLength)
+        {
+            Array.Reverse(dataLength);
+            int pos = 0;
+            uint result = 0;
+            foreach(byte by in dataLength)
+            {
+                result |= (uint)(by << pos);
+                 pos += 8;
+            }
+               return result;
+        }
 
 
-        public string getCommandType(byte cmdByte)
+
+        private string getCommandType(byte cmdByte)
         {
             bool commandResponseBit;
             bool writeReadBit;
@@ -98,13 +189,24 @@ namespace Star_Dundee_WPF.Models
             return command;
         }
 
-        public bool getBit(byte cmdByte, int index)
+        private bool getBit(byte cmdByte, int index)
         {
             bool bit = (cmdByte & (1 << index-1)) != 0;
             return bit;
         }
 
+        void printPacketDetails(RMAP packet)
+        {
+            if(packet.command.Equals("WRITE"))
+            {
+                Console.Write("COMMAND: {0:x}/{1}\nDESTINATION KEY: {2:x}\nSOURCE LOGICAL ADDRESS: {3:x}\nTRANSACTION IDENTIFIER: {4:x}",packet.ptCmdSp,packet.command,packet.destinationKey,packet.sourceLAdd,packet.transactionID[1]);
+            }
+        }
+
     }
+
+    
+
 
     
 

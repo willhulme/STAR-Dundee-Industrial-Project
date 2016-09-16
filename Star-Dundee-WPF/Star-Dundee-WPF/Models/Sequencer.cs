9f9c4522ec1b3d41ecad4ddebc9a5193cdd09684
Matt Malone <mmalone136@gmail.com>
2016-09-16 15:05:57 +0100

    FileParser and Sequencer files tidied and commented

--- Star-Dundee-WPF/Star-Dundee-WPF/Models/Sequencer.cs
+++ Star-Dundee-WPF/Star-Dundee-WPF/Models/Sequencer.cs
@@ -14,33 +14,21 @@ namespace Star_Dundee_WPF.Models
             List<string[]> dataSets = new List<string[]>();
             int dataSetLength;
 
+            //Loop for each packet in list
             foreach (Packet current in p)
             {
+                //Add the data section to list of data sets
                 dataSets.Add(current.getData().getTheData());
             }
 
+            //Get length of data string
             dataSetLength = dataSets[0].Count();
 
-
-
-
+            //Call to function to get hex values converted to decimal
             List<int[]> convertedData = getDecValues(dataSets, dataSetLength);
 
-
-            //for (int i = 0; i < convertedData.Count(); i++)
-           // {
-             //   for (int j = 0; j < dataSetLength; j++)
-             //   {
-              //      Console.Write(" " + convertedData[i][j]);
-              //  }
-             //   Console.WriteLine("");
-          //  }
-
-
+            //Function call to get the sequence index
             int result = getTheSequenceIndex(convertedData);
-            Console.WriteLine("");
-            Console.WriteLine(result);
-            Console.WriteLine("");
             return result;
 
         }
@@ -48,36 +36,37 @@ namespace Star_Dundee_WPF.Models
 
         public int getTheSequenceIndex(List<int[]> theData)
         {
-
-
+            //Create list to store possible index values
             List<int> possibleIndex = new List<int>();
-            int sequenceIndex;
 
-
+            //Set initial current and previous values for data comparison
             int[] prev = theData[0];
             int[] curr = theData[1];
 
-            //Compare first two lines of data to find potential sequence number index
+            //Compare only first two lines of data to find potential sequence number index
             for (int i = 0; i < theData[0].Count(); i++)
             {
-
+                //Check if current is exactly 1 more than previous
                 if (curr[i] == (prev[i] + 1))
                 {
+                    //If so add to list for possible index
                     possibleIndex.Add(i);
                 }
-
             }
 
 
-
+            //Loop through the remaining lines of data
             for (int i = 2; i < theData.Count(); i++)
             {
+                //Set current and previous lines for comparing
                 curr = theData[i];
                 prev = theData[i - 1];
 
+                //For every possible index identified
                 for (int x = 0; x < possibleIndex.Count(); x++)
                 {
                     int currIndex = possibleIndex[x];
+                    //Check next line for increment
                     if (curr[currIndex] == (prev[currIndex] + 1))
                     {
                         //Still could be the index
@@ -85,19 +74,20 @@ namespace Star_Dundee_WPF.Models
                     }
                     else
                     {
+                        //Incremented value not found so remove from list of possibilities
                         possibleIndex.Remove(currIndex);
                     }
-
                 }
-
             }
 
+            //If there is only one possible sequence index left, return it as the index
             if (possibleIndex.Count() == 1)
             {
                 return possibleIndex[0];
 
             }
             else {
+                //Return if there is less or more than one possibility
                 return -1;
             }
 
@@ -106,31 +96,24 @@ namespace Star_Dundee_WPF.Models
 
         public List<int[]> getDecValues(List<string[]> dataSets, int dataSetLength)
         {
-
             List<int[]> converted = new List<int[]>();
 
+            //Loop through each line of data in the recording
             foreach (string[] currSet in dataSets)
             {
                 List<int> intList = new List<int>();
+                //Loop through each string in the current line
                 foreach (string currString in currSet)
                 {
+                    //Convert hex string into int value and store in list
                     int x = Convert.ToInt32(currString, 16);
-                    //Console.WriteLine(" - " + currString + " - " + x + " - ");
                     intList.Add(x);
                 }
+                //Add converted line of data to overall list
                 converted.Add(intList.ToArray());
             }
-            Console.WriteLine("");
 
-            //for (int i = 0; i < converted.Count(); i++)
-            //{
-            //    for (int j = 0; j < dataSetLength; j++)
-            //    {
-                    //Console.Write(" " + converted[i][j]);
-            //    }
-            //    Console.WriteLine("");
-           // }
-            Console.WriteLine("\n DONE \n");
+            //Return the list of decimal values
             return converted;
         }
     }

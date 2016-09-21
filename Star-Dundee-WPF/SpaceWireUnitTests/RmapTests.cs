f084be89d63164a703c6a52a04068286caa2c1c0
RyanRobinson <RyanRobinson@computing.dundee.ac.uk>
2016-09-21 13:32:00 +0100

    Added first unit test method

--- Star-Dundee-WPF/SpaceWireUnitTests/RmapTests.cs
+++ Star-Dundee-WPF/SpaceWireUnitTests/RmapTests.cs
@@ -10,8 +10,13 @@ namespace SpaceWireUnitTests
         [TestMethod]
         public void TestRMAPReadPathAddress()
         {
+            string cargo = "4d 20 00 00 03 02 fe 00 01 00 00 00 01 00 00 00 04 f0";
+            RMAP rmap = new RMAP();
+            rmap.buildPacket(cargo);
 
-            RMAP rmap = new RMAP();           
+            string command = rmap.command;
+
+            Assert.AreEqual("READ", command);
         }
     }
 }
